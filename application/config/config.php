9d1f4202165bb8624f3dd5cd5262785995a1d61f
Loups <s99113154@stu.edu.tw>
2013-11-24 22:20:22 +0800

    add file

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@
 |
 */
 
-$config['base_url']	= 'http://localhost/dysarthria-scale-php/';
+$config['base_url']	= 'http://120.119.54.47/dysarthria-scale-php/';
 
 /*
 |--------------------------------------------------------------------------
