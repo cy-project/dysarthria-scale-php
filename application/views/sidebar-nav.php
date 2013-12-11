343acdfee93bab2db844f9b0f005057c9b2f96b9
Loups <s99113154@stu.edu.tw>
2013-12-11 11:28:23 +0800

    edit url about not found

--- application/views/sidebar-nav.php
+++ application/views/sidebar-nav.php
@@ -19,12 +19,12 @@
         <a href="#accounts-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-folder-open"></i>專案</i></a>
         <ul id="accounts-menu" class="nav nav-list collapse in">
             <li ><a href="<?=base_url("/projectadmin/project_home")?>">專案管理</a></li>
-            <li ><a href="users-testlist.html">受測名單</a></li>
+            <li ><a href="#">受測名單</a></li>
         </ul>
 
         <a href="#error-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-search"></i>追蹤</a>
         <ul id="error-menu" class="nav nav-list collapse in">
-            <li ><a href="users-track.html">追蹤名單</a></li>
+            <li ><a href="#">追蹤名單</a></li>
         </ul>
 
         <a href="#legal-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-th-list"></i>圖表</a>
