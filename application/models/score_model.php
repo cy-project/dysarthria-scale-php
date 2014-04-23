54b5bee23dac975b2dc55c47689657447380c2d2
jasdef <ja17272001@yahoo.com.tw>
2014-04-23 14:46:11 +0800

    score

--- application/models/score_model.php
+++ application/models/score_model.php
@@ -531,29 +531,42 @@ result.testing_id =  '$testing_id' and(".$str.")";
 	if($office_id==0){ //實習生
 	
 	$select_sql="SELECT
-project.`name` as pname,
-children.`name`as cname,
-topic.title,
-group_concat(topic.script) AS script,
-group_concat(result.voice_file) AS voice_file,
-group_concat(judgment.wrongcode) as wrongcode,
-topic.part,
-group_concat(judgment.note) as note
+test_results_all_list.pname,
+test_results_all_list.cname,
+test_results_all_list.title,
+group_concat(test_results_all_list.script Order By test_results_all_list.script ASC ) AS script,
+group_concat(test_results_all_list.voice_file) AS voice_file,
+group_concat(test_results_all_list.wrongcode  Order By test_results_all_list.wrongcode ASC) AS wrongcode,
+test_results_all_list.part,
+group_concat(test_results_all_list.note) AS note
 FROM
-project
-INNER JOIN testing_list AS testings ON testings.project_id = project.id
-INNER JOIN result ON result.testing_id = testings.id
-INNER JOIN topic ON result.topic_id = topic.id
-INNER JOIN judg_list ON judg_list.result_id = result.id
-INNER JOIN judgment ON judg_list.judgment_id = judgment.id
-INNER JOIN children ON testings.children_id = children.id
+test_results_all_list
 WHERE
-testings.id = '".$testing_list_id."' AND
-project.id = '".$project_id."' AND
-judgment.available = '".$office_id."'
+test_results_all_list.testing_id =  '$testing_list_id' AND
+test_results_all_list.project_id =  '$project_id' AND
+test_results_all_list.available =  '$office_id' AND
+test_results_all_list.part ='1'
 GROUP BY
-topic.title
+test_results_all_list.title
 ";
+/*	
+$select_sql="SELECT
+test_results_all_list.pname as pname,
+test_results_all_list.cname as cname,
+test_results_all_list.title as title,
+group_concat(test_results_all_list.script ORDER BY test_results_all_list.script  ASC ) as script,
+group_concat(test_results_all_list.voice_file) as voice_file,
+group_concat(test_results_all_list.wrongcode) as wrongcode,
+test_results_all_list.part as part,
+group_concat(test_results_all_list.note) as note
+FROM
+test_results_all_list
+WHERE
+test_results_all_list.testing_id =  '".$testing_list_id."' AND
+test_results_all_list.project_id =  '".$project_id."' AND
+test_results_all_list.available =  '".$office_id."'
+GROUP BY
+test_results_all_list.title";*/
 	
 	}elseif($office_id==1){ //語言治療師
 	
@@ -561,7 +574,7 @@ topic.title
 project.`name` as pname,
 children.`name`as cname,
 topic.title,
-group_concat(topic.script) AS script,
+group_concat(topic.script ORDER BY topic.script ASC ) AS script,
 group_concat(result.voice_file) AS voice_file,
 group_concat(judgment.wrongcode) as wrongcode,
 topic.part,
@@ -580,8 +593,27 @@ project.id = '".$project_id."' AND
 judgment.available = '".$office_id."'
 GROUP BY
 topic.title
+ORDER BY
+topic.part ASC,
+topic.id ASC
 ";
-	
+	/*$select_sql="SELECT
+test_results_all_list.pname as pname,
+test_results_all_list.cname as cname,
+test_results_all_list.title as title,
+group_concat(test_results_all_list.script ORDER BY test_results_all_list.script  ASC ) as script,
+group_concat(test_results_all_list.voice_file) as voice_file,
+group_concat(test_results_all_list.wrongcode) as wrongcode,
+test_results_all_list.part as part,
+group_concat(test_results_all_list.note) as note
+FROM
+test_results_all_list
+WHERE
+test_results_all_list.testing_id =  '".$testing_list_id."' AND
+test_results_all_list.project_id =  '".$project_id."' AND
+test_results_all_list.available =  '".$office_id."'
+GROUP BY
+test_results_all_list.title";*/
 	}
 
 	
@@ -614,6 +646,30 @@ project.id = '".$project_id."'
 	return $query;
 	
 	
+	}
+	
+	public function score_intern_speech_ajax_2($testing_list_id,$member_id,$project_id,$office_id){
+	
+	$select_sql="SELECT
+	test_results_all_list.pname,
+	test_results_all_list.cname,
+	test_results_all_list.title,
+	test_results_all_list.script   AS script,
+	test_results_all_list.voice_file AS voice_file,
+	test_results_all_list.wrongcode  AS wrongcode,
+	test_results_all_list.part,
+	test_results_all_list.note AS note
+	FROM
+	test_results_all_list
+	WHERE
+	test_results_all_list.testing_id =  '$testing_list_id' AND
+	test_results_all_list.project_id =  '$project_id' AND
+	test_results_all_list.available =  '$office_id' AND
+	test_results_all_list.part <> '1'";
+
+	$query =$this->db->query($select_sql); //select
+	
+	return $query;
 	}
 	
 }
--- application/views/score_intern_evaluation_ajax.php
+++ application/views/score_intern_evaluation_ajax.php
@@ -27,8 +27,6 @@
 		
 			<?php
 			
-			
-			
 			if($row->part==1){
 			
 			$script_array=explode(",",$row->script);
@@ -77,20 +75,11 @@
 			
 			}
 			
-			//print_r($note);
-		
-			
-		
-			
-			
-			
 			for($i=0;$i<count($script_array);$i++){
-			
-				
-							  
+					  
 							  if($wrongcode_array[$i]=="1"){
 							  
-									$s2="<td>".$row->title."</td>";
+									$s2="<tr><td>".$row->title."</td>";
 									
 							  
 							  }elseif($wrongcode_array[$i]=="0"){
@@ -113,90 +102,27 @@
 			}
 			
 			
-			}else{  //-------------------------
+			}
+			endforeach;
+			
+			
+			?>
+			
+			<?php
+			
+			
+			foreach($intern2->result() as $row):
+			
+			
+			if($row->part != 4){
+				$script_array=explode(" ",$row->script);
+			
 				
-				$s9="";
-				$s10="";
-				$script_array=explode(",",$row->script); 
-				
-				$wrongcode_array=explode(",",$row->wrongcode);
-				
-				$note=explode(",",$row->note);
-				
-				$note_str="";
-				for($d=0;$d<count($note);$d++){
-				
-				if($note[$d]!=""){
-				
-					$note_str.=$note[$d]."<br>";
-
 				}else{
 				
-					$note_str.=",";
-				
-				}
-			
-			}
-			
-			$note_str=explode(",",$note_str);
-			$note="";
-				for($r=0;$r<count($note_str);$r++){
-			
-				if($note_str[$r]!=""){
-				
-				$note[$r]=$note_str[$r];
-				
-				}
-			
-			}
-				
-				
-				$wrongcode_arrays="";
-				$asd_array="";
-				$x=0;
-				
-				for($j=0;$j<count($wrongcode_array);$j++){
-					
-					if($wrongcode_array[$j]==""){
-					
-					$wrongcode_arrays[$x]=$asd_array;
-					$x++;
-					$asd_array="";
-					}else{
-					
-						$asd_array.=$wrongcode_array[$j].",";
-					
-					}
-							
-				}
-				
-				
-				
-				$voice_file_array=explode(",",$row->voice_file);
-						 
-				
-				
-				for($j=0;$j<count($script_array);$j++){
-				
-				
-				
-				
-				
-				$s7="<td>".$row->title."</td>";
-				
-				
-				
-				if($row->part != 4){
-					
-					
-					$script_array2=explode(" ",$script_array[$j]);
-					
-					
-							}else{
-							
-
 					$strs="";
-					$script_array3=explode(" ",$script_array[$j]);
+					
+					$script_array3=explode(" ",$row->script);
 					
 					$t=0;
 					$h=1;
@@ -216,58 +142,87 @@
 					
 					}
 					
-					$script_array2=explode(",",$strs);
-					
-					//print_r($script_array2);
-					}
-					
-					
-					
-					$wrongcode_array=explode(",",$wrongcode_arrays[$j]);
-					
-					for($i=0;$i<count($script_array2);$i++){
-					
-						if($wrongcode_array[$i]=="1"){
+					$script_array=explode(",",$strs);
+				
+				
+				
+			}
+			
+			$wrongcode_array=explode(",",$row->wrongcode);
+			
+			$wrongcode_array2="";
+			
+			for($i=0;$i<count($wrongcode_array);$i++){
+			
+				if($i == (count($wrongcode_array)-2)){
+				
+				$wrongcode_array2.=$wrongcode_array[$i];
+				
+						}else if($i < (count($wrongcode_array)-2)){
+				
+				$wrongcode_array2.=$wrongcode_array[$i].",";
+				}
+			
+			}
+			
+			$wrongcode_array=explode(",",$wrongcode_array2);
+		
+			$s10="";
+			
+				
+			for($i=0;$i<count($script_array);$i++){
+				
+				 if($wrongcode_array[$i]=="1"){
 							  
-									$s10.=$script_array2[$i]."<br>";
+									$s10.=$script_array[$i]."<br>";
 									
 							  
 							  }elseif($wrongcode_array[$i]=="0"){
 											
-									$s10.="<span class=\"label label-success\" title=\"不清楚\">".$script_array2[$i]."</span><br>";
+									$s10.="<span class=\"label label-success\" title=\"不清楚\">".$script_array[$i]."</span><br>";
 											
 							  }elseif($wrongcode_array[$i]=="-1"){
 									
-									$s10.="<span class=\"label label-warning\" title=\"不正確\">".$script_array2[$i]."</span><br>";
+									$s10.="<span class=\"label label-warning\" title=\"不正確\">".$script_array[$i]."</span><br>";
 							  
 						}
-					}
-					
-					
-					
-					
-					
-					$s9.="<td>".$s10."</td><td>".$note[$j]."</td><td >
-					<div class=\"wavclass\" id=\"wavshow-".$temp."\"></div>
-						  <input type=\"hidden\" id=\"wavget-".$temp."\" value=\"".$voice_file_array[$j]."\" />"."</td></tr>";
-					
-					$Stringall2.=$s7.$s9;	
-					
-					$s9="";
-					$s10="";
-					$temp++;
-				}
-
+			
 			
 			}
 			
-			?>
+				
+		
 			
-			<?php endforeach;
+			$note=explode(",",$row->note);
+				
+				$note_str="";
+				for($d=0;$d<count($note);$d++){
+				
+				if($note[$d]!=""){
+				
+					$note_str.=$note[$d]."<br>";
+
+				}
+			
+			}
+			
+			
+			$Stringall2.="<tr><td>".$row->title."</td>".
+			"<td>".$s10."</td>".
+			"<td>".$note_str."</td>".
+			"<td><div class=\"wavclass\" id=\"wavshow-".$temp."\"></div>
+				<input type=\"hidden\" id=\"wavget-".$temp."\" value=\"".$row->voice_file."\" /></td></tr>"; 
+			
+			
+			
+			
+			$temp++;
+			
+			endforeach;
 			
 			echo $Stringall.$Stringall2;
-			
 			?>
+
 </tbody>
 </table>
 
